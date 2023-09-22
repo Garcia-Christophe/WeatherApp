3fb9b485b369a290333c261e936e8eb8efc9e92b
G.Christophe <garciachristophe29@gmail.com>
2023-09-22 15:30:10 +0200

    build: config gradle and swagger

---
+++ weather/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+CREATE TABLE City (
+    id VARCHAR(22),
+    name VARCHAR(22),
+    code_postal NUMBER(5),
+    region VARCHAR(22),
+    country VARCHAR(22)
+);
+
+INSERT INTO City (id, name, code_postal, region, country) VALUES ('1', 'Paris', 75056, 'Île de France', 'France');
+INSERT INTO City (id, name, code_postal, region, country) VALUES ('2', 'Barcelone', 08001, 'Catalogne', 'Espagne');
+INSERT INTO City (id, name, code_postal, region, country) VALUES ('3', 'Venise', 30100, 'Vénétie', 'Italie');
+INSERT INTO City (id, name, code_postal, region, country) VALUES ('4', 'Dakar', 12500, 'Dakar', 'Sénégal');
+INSERT INTO City (id, name, code_postal, region, country) VALUES ('5', 'Librilla', 30892, 'Murcia', 'Espagne');
